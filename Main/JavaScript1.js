6a105a5212ce3bf42eb9954670338deeeadfbce8
brandoncabael <brandon.cabael@gmail.com>
2017-05-21 05:53:05 -0600

    This is to rework the file structure

---
+++ Main/JavaScript1.js
@@ -0,0 +1,33 @@
+﻿const Cmr1Cli = require('cmr1-cli');
+
+
+const myCli = new Cmr1Cli({
+    name: 'My CLI script name',
+    description: 'A description of this script',
+    helpHeader: 'Available Options',
+    });
+
+
+let test = myCli.prompt("Does this work? ")
+myCli.log(`welcome ${test}`)
+
+const options = ["test", "poop", "slack"]
+let selection
+
+var step1 = function () {
+    selection = myCli.select(options, "Select one of the elements... ")
+    myCli.log(`You selected ${options[selection]}`)
+}
+
+
+do {
+    step1()
+} while (!myCli.confirm("Is this your final answer?"))
+
+if (selection === 0) {
+    myCli.success("You are correct!")
+}
+else {
+    myCli.warn("Suck it!")
+}
+myCli.log(myCli.options)
